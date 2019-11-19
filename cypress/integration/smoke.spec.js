cb874deb20dc5f85c552d2d227a950b9365a3cfa
Sanz Martinez <mosn@gft.com>
2019-11-19 11:50:17 +0100

    first commit

---
+++ cypress/integration/smoke.spec.js
@@ -0,0 +1,8 @@
+describe ('smoke Test', () =>{
+    it ( 'Makes sure the welcome message comes up!', () => {
+
+        cy.visit('http://localhost:1234')
+          .contains('Hello, World!')
+
+    });
+});
